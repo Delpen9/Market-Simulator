bf9a68440c5aed3754747cddb843a4975419bc9b
Delpen9 <iantdover@gmail.com>
2023-01-02 14:34:39 -0600

    Delete testproject.py

--- testproject.py
+++
@@ -1,9 +0,0 @@
-from indicators import run_indicators
-from TheoreticallyOptimalStrategy import run_theoretically_optimal_strategy
-
-def run():
-    run_indicators()
-    run_theoretically_optimal_strategy()
-    
-if __name__ == "__main__":
-	run()
