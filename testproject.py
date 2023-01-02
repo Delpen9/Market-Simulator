1089c896fb62b60a90d2734466a8d1c92ce93681
Delpen9 <iantdover@gmail.com>
2023-01-02 14:33:05 -0600

    Add files via upload

---
+++ testproject.py
@@ -0,0 +1,9 @@
+from indicators import run_indicators
+from TheoreticallyOptimalStrategy import run_theoretically_optimal_strategy
+
+def run():
+    run_indicators()
+    run_theoretically_optimal_strategy()
+    
+if __name__ == "__main__":
+	run()
